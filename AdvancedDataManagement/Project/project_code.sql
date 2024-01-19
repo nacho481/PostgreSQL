40acb3ce871b1ddd9d58af84768ee82327a644fd
Nacho <nachitoatilano@gmail.com>
2024-01-19 11:02:24 -0800

    Comments
    I explained a lot of the logic within the code.

--- AdvancedDataManagement/Project/project_code.sql
+++ AdvancedDataManagement/Project/project_code.sql
@@ -2,15 +2,23 @@
 
 -- Summary table of genere and total-
 SELECT 
-	category.name AS genre,
-	SUM(payment.amount) AS total_revenue
+	category.name AS genre,  -- get genre 
+	SUM(payment.amount) AS total_revenue  -- find total_revenue
 FROM inventory 
+	-- link (inventory) to (rental) table
 INNER JOIN rental ON rental.inventory_id = inventory.inventory_id
+	-- link (rental) to (payment) table
 INNER JOIN payment ON payment.rental_id = rental.rental_id
+	-- get genere's through the following tables 
+	-- inventory -> film -> film_category -> category
+	-- link (inventory) to (film)
 INNER JOIN film	ON film.film_id = inventory.film_id
+	-- link (film) to (film_category)
 INNER JOIN film_category ON film_category.film_id = film.film_id
+	-- link (film_category) to (category)
 INNER JOIN category ON category.category_id = film_category.category_id
 GROUP BY genre
+	-- descending order put's top earners at top! 
 ORDER BY total_revenue DESC;
 
 
