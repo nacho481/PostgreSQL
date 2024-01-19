6d12ed59b784901b918d381b133d64bfb1054e7d
Nacho <nachitoatilano@gmail.com>
2024-01-18 18:23:30 -0800

    Update project_code.sql
    Top query is a detailed query joining several tables. I joined the film_category and category so I can access the genre's or the category.name, I put that under an alias as genre. The only way to access that was to go to Film -> Film_Category -> Category so that was like 2 Inner Joins but whatever.
    
    I grouped it by inventory id firstly then I specified that I wanted the HAVING COUNT or the sales need to be more or equal to than 5.
    
    The second query is just the summary, a lot less is going on . I'm just querying for the sale information, it's not that bad.

--- AdvancedDataManagement/Project/project_code.sql
+++ AdvancedDataManagement/Project/project_code.sql
@@ -1,11 +1,34 @@
-SELECT * FROM film LIMIT 5;
-SELECT * FROM inventory LIMIT 5; 
-SELECT * FROM film_category LIMIT 5;
-SELECT * FROM category LIMIT 5;
+SELECT 
+	inventory.inventory_id, 
+	film.film_id, 
+	film.release_year AS release_date,
+	category.name AS genre, 
+	COUNT(rental.rental_id) AS number_of_rentals
+FROM inventory 
+INNER JOIN rental 
+	ON rental.inventory_id = inventory.inventory_id
+INNER JOIN film 
+	ON film.film_id = inventory.film_id
+INNER JOIN film_category 
+	ON film_category.film_id = film.film_id
+INNER JOIN category 
+	ON category.category_id = film_category.category_id
+GROUP BY 
+	inventory.inventory_id, 
+	film.film_id, 
+	release_date,
+	genre, 
+	film_category.film_id
+HAVING COUNT(rental.rental_id) >= 5
+ORDER BY number_of_rentals;
 
--- figure out how many times a movie was rented
-SELECT inventory.inventory_id, COUNT(rental.rental_id) AS number_of_rentals
+-- summary table 
+SELECT 
+	inventory.inventory_id, 
+	COUNT(rental.rental_id) AS number_of_rentals
 FROM inventory
-INNER JOIN rental ON rental.inventory_id = inventory.inventory_id
+INNER JOIN rental 
+	ON rental.inventory_id = inventory.inventory_id
 GROUP BY inventory.inventory_id
+HAVING COUNT(rental.rental_id) >= 5
 ORDER BY number_of_rentals DESC;
