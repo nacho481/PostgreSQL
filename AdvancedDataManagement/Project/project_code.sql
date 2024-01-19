005b2769cc57337aa98ded8b8cb8500bbc3c3bbf
Nacho <nachitoatilano@gmail.com>
2024-01-19 10:56:52 -0800

    Deleted old code
    Scrapped the old project idea. Now, this project will focus on what genre's make the most amount of money!

--- AdvancedDataManagement/Project/project_code.sql
+++ AdvancedDataManagement/Project/project_code.sql
@@ -1,29 +1,6 @@
-SELECT 
-	inventory.inventory_id, 
-	film.film_id, 
-	film.title,
-	film.release_year AS release_date,
-	category.name AS genre, 
-	COUNT(rental.rental_id) AS number_of_rentals
-FROM inventory 
-INNER JOIN rental 
-	ON rental.inventory_id = inventory.inventory_id
-INNER JOIN film 
-	ON film.film_id = inventory.film_id
-INNER JOIN film_category 
-	ON film_category.film_id = film.film_id
-INNER JOIN category 
-	ON category.category_id = film_category.category_id
-GROUP BY 
-	inventory.inventory_id, 
-	film.film_id, 
-	release_date,
-	genre, 
-	film_category.film_id
--- HAVING COUNT(rental.rental_id) >= 5
-ORDER BY number_of_rentals DESC;
 
--- Summary table of genere and total_revenue 
+
+-- Summary table of genere and total-
 SELECT 
 	category.name AS genre,
 	SUM(payment.amount) AS total_revenue
@@ -36,22 +13,5 @@ INNER JOIN category ON category.category_id = film_category.category_id
 GROUP BY genre
 ORDER BY total_revenue DESC;
 
-SELECT COUNT(*) AS number_of_documentaries
-FROM film
-INNER JOIN film_category 
-	ON film.film_id = film_category.film_id
-INNER JOIN category 
-	ON category.category_id = film_category.category_id
-WHERE category.name = 'Documentary';
 
 
--- summary table 
-SELECT 
-	inventory.inventory_id, 
-	COUNT(rental.rental_id) AS number_of_rentals
-FROM inventory
-INNER JOIN rental 
-	ON rental.inventory_id = inventory.inventory_id
-GROUP BY inventory.inventory_id
-HAVING COUNT(rental.rental_id) >= 5
-ORDER BY number_of_rentals DESC;
