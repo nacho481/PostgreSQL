6f570be34b1916d9590e123b501f73f52479abc2
Nacho <nachitoatilano@gmail.com>
2024-01-19 14:41:49 -0800

    Update project_code.sql
    Updated the detailed table. Also, refactored the summary table.

--- AdvancedDataManagement/Project/project_code.sql
+++ AdvancedDataManagement/Project/project_code.sql
@@ -3,6 +3,7 @@
 -- Summary table of genere and total-
 SELECT 
 	category.name AS genre,  -- get genre 
+	COUNT(rental.rental_id) as number_of_rentals,
 	SUM(payment.amount) AS total_revenue  -- find total_revenue
 FROM inventory 
 	-- link (inventory) to (rental) table
@@ -19,7 +20,32 @@ INNER JOIN film_category ON film_category.film_id = film.film_id
 INNER JOIN category ON category.category_id = film_category.category_id
 GROUP BY genre
 	-- descending order put's top earners at top! 
+ORDER BY total_revenue DESC
+LIMIT 20;
+
+SELECT
+    film.title,
+    film.release_year AS release_date,
+    category.name AS genre,
+    COUNT(rental.rental_id) AS number_of_rentals,
+    SUM(payment.amount) AS total_revenue
+FROM inventory
+	-- link (inventory) to (rental) table
+INNER JOIN rental ON rental.inventory_id = inventory.inventory_id
+	-- link (rental) to (payment) table
+INNER JOIN payment ON payment.rental_id = rental.rental_id
+	-- get genere's through the following tables 
+	-- inventory -> film -> film_category -> category
+	-- link (inventory) to (film)
+INNER JOIN film ON film.film_id = inventory.film_id
+	-- link (film) to (film_category)
+INNER JOIN film_category ON film_category.film_id = film.film_id
+	-- link (film_category) to (category)
+INNER JOIN category 
+	ON category.category_id = film_category.category_id
+GROUP BY 
+	film.title, 
+	film.release_year, 
+	category.name
 ORDER BY total_revenue DESC;
-
-
 
