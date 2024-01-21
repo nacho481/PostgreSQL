955ae37e8034cf6b8c9d1f1c2acd34cb768364b0
Nacho <nachitoatilano@gmail.com>
2024-01-21 14:53:16 -0800

    Update project_code.sql
    Put the detailed table SELECT query above the summary table. Also, creating the table syntax for the detailed table.

--- AdvancedDataManagement/Project/project_code.sql
+++ AdvancedDataManagement/Project/project_code.sql
@@ -1,27 +1,10 @@
+-- ==================== CREATE - Detailed Table ==================== 
+CREATE TABLE IF NOT EXISTS genre_sales(
+	genre VARCHAR(50),
+	
+	
+);
 
-
--- Summary table of genere and total-
-SELECT 
-	category.name AS genre,  -- get genre 
-	COUNT(rental.rental_id) as number_of_rentals,
-	SUM(payment.amount) AS total_revenue  -- find total_revenue
-FROM inventory 
-	-- link (inventory) to (rental) table
-INNER JOIN rental ON rental.inventory_id = inventory.inventory_id
-	-- link (rental) to (payment) table
-INNER JOIN payment ON payment.rental_id = rental.rental_id
-	-- get genere's through the following tables 
-	-- inventory -> film -> film_category -> category
-	-- link (inventory) to (film)
-INNER JOIN film	ON film.film_id = inventory.film_id
-	-- link (film) to (film_category)
-INNER JOIN film_category ON film_category.film_id = film.film_id
-	-- link (film_category) to (category)
-INNER JOIN category ON category.category_id = film_category.category_id
-GROUP BY genre
-	-- descending order put's top earners at top! 
-ORDER BY total_revenue DESC
-LIMIT 20;
 
 -- Detailed table 
 SELECT
@@ -49,6 +32,33 @@ GROUP BY
 	film.release_year, 
 	category.name
 ORDER BY total_revenue DESC;
+
+
+
+
+-- Summary table of genere and total-
+SELECT 
+	category.name AS genre,  -- get genre 
+	COUNT(rental.rental_id) as number_of_rentals,
+	SUM(payment.amount) AS total_revenue  -- find total_revenue
+FROM inventory 
+	-- link (inventory) to (rental) table
+INNER JOIN rental ON rental.inventory_id = inventory.inventory_id
+	-- link (rental) to (payment) table
+INNER JOIN payment ON payment.rental_id = rental.rental_id
+	-- get genere's through the following tables 
+	-- inventory -> film -> film_category -> category
+	-- link (inventory) to (film)
+INNER JOIN film	ON film.film_id = inventory.film_id
+	-- link (film) to (film_category)
+INNER JOIN film_category ON film_category.film_id = film.film_id
+	-- link (film_category) to (category)
+INNER JOIN category ON category.category_id = film_category.category_id
+GROUP BY genre
+	-- descending order put's top earners at top! 
+ORDER BY total_revenue DESC
+LIMIT 20;
+
 
 
 
