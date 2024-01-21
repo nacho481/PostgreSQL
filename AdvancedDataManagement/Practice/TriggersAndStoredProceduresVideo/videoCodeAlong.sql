dd1bbae40df678f13eda18d8f4366ae571cbbdc3
Nacho <nachitoatilano@gmail.com>
2024-01-20 17:16:57 -0800

    Update videoCodeAlong.sql
    Made new trigger tested out some data and working on new function and new trigger too.

--- AdvancedDataManagement/Practice/TriggersAndStoredProceduresVideo/videoCodeAlong.sql
+++ AdvancedDataManagement/Practice/TriggersAndStoredProceduresVideo/videoCodeAlong.sql
@@ -34,13 +34,59 @@ RETURNS TRIGGER
 LANGUAGE PLPGSQL
 AS $$
 BEGIN
+	-- we want to update our large sales by channel table easiest way is to delete the table, and reinsert all + new data
+	-- clear data from this table 
+	DELETE FROM large_sales_by_channel;
+	INSERT INTO large_sales_by_channel
+		SELECT channel, COUNT(customer_id)
+		FROM large_sales
+		GROUP BY channel;
 	
+	RETURN NEW;
 END;
 $$
-
 
 CREATE TRIGGER new_large_sale 
 	AFTER INSERT
 	ON large_sales
 	FOR EACH STATEMENT
-	EXECUTE PROCEDURE insert_trigger_function(); 
+	EXECUTE PROCEDURE insert_trigger_function();
+	
+SELECT COUNT(*) FROM large_sales;
+SELECT SUM(number_of_sales) FROM large_sales_by_channel;
+
+INSERT INTO large_sales VALUES('call center', 57, 125000);
+
+SELECT COUNT(*) FROM large_sales;
+SELECT SUM(number_of_sales) FROM large_sales_by_channel;
+SELECT * FROM large_sales_by_channel; 
+
+
+-- update our large sales by channel 
+CREATE OR REPLACE FUNCTION update_trigger_function()
+RETURNS TRIGGER 
+LANGUAGE PLPGSQL
+AS $$
+BEGIN 
+	DELETE FROM large_sales_by_channel;
+	INSERT INTO large_sales_by_channel
+	SELECT channel, COUNT(customer_id)
+	FROM large_sales
+	GROUP BY channel;
+	
+	RETURN NEW;
+END;
+$$;
+
+CREATE TRIGGER large_sales_update 
+AFTER UPDATE 
+ON large_sales 
+FOR EACH STATEMENT 
+EXECUTE PROCEDURE update_trigger_function(); 
+
+SELECT * FROM large_sales WHERE customer_id > 20000;
+SELECT COUNT(*) FROM large_sales WHERE customer_id > 20000;
+-- we'll update customers with a higher ID to VIP 
+SELECT * FROM large_sales_by_channel;
+
+UPDATE large_sales SET channel = 'VIP' WHERE customer_id > 20000;
