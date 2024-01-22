2d5a3a9a6a5e371d603ffc5b901ea733447ca67e
Nacho <nachitoatilano@gmail.com>
2024-01-22 10:59:32 -0800

    Refactor detailed_table
    So I changed the NUMERIC from
    NUMERIC(2,2) to
    NUMERIC(4,2)
    
    At most, we'll get
    $xx.xx
    So we only need a length of 4. Recall, the 2nd parameter for NUMERIC will specify how many decimal points you want.
    
    I also modified the SELECT statement to cast the conversion with the data type that I mentioned above. Finally, I also decided to do a LEFT JOIN per the video on "Triggers and Procedures." He makes a good point, I wouldn't want to potentially miss any records on the LEFT table if it can relate to revenue. I'll see if my SELECT query corresponding to the summary table changes when I replace the INNER JOINS with LEFT JOINS.
    
    Also, I set up the shell to insert data into the detailed_table and the summary_table

--- AdvancedDataManagement/Project/project_code.sql
+++ AdvancedDataManagement/Project/project_code.sql
@@ -5,8 +5,12 @@ CREATE TABLE IF NOT EXISTS genre_sales(
 	rental_id BIGINT,
 	-- this creates a MONEY data type explicitly stating to only allow 2 to the left and 
 	-- 2 floating points
-	payment NUMERIC(2,2)::MONEY 
+	payment NUMERIC(4,2)::MONEY 
 );
+
+-- ==================== INSERT INTO - Detailed Table ==================== 
+
+
 
 -- ==================== CREATE - Summary Table ==================== 
 CREATE TABLE IF NOT EXISTS genre_sales_summary(
@@ -16,26 +20,28 @@ CREATE TABLE IF NOT EXISTS genre_sales_summary(
 	total_revenue NUMERIC(6,2)::MONEY
 );
 
+-- ==================== INSERT INTO - Summary Table ==================== 
+
 
 -- Detailed table 
 SELECT
     film.title,
     category.name AS genre,
     COUNT(rental.rental_id) AS number_of_rentals,
-    SUM(payment.amount) AS total_revenue
+    (SUM(payment.amount))::NUMERIC(4,2)::MONEY AS total_revenue
 FROM inventory
 	-- link (inventory) to (rental) table
-INNER JOIN rental ON rental.inventory_id = inventory.inventory_id
+LEFT JOIN rental ON rental.inventory_id = inventory.inventory_id
 	-- link (rental) to (payment) table
-INNER JOIN payment ON payment.rental_id = rental.rental_id
+LEFT JOIN payment ON payment.rental_id = rental.rental_id
 	-- get genere's through the following tables 
 	-- inventory -> film -> film_category -> category
 	-- link (inventory) to (film)
-INNER JOIN film ON film.film_id = inventory.film_id
+LEFT JOIN film ON film.film_id = inventory.film_id
 	-- link (film) to (film_category)
-INNER JOIN film_category ON film_category.film_id = film.film_id
+LEFT JOIN film_category ON film_category.film_id = film.film_id
 	-- link (film_category) to (category)
-INNER JOIN category 
+LEFT JOIN category 
 	ON category.category_id = film_category.category_id
 GROUP BY 
 	film.title, 
